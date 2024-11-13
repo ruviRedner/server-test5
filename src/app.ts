a5cb4474b74f7461fc88a5749b685fad945f820e
ruviRedner <r.y.r7966@gmail.com>
2024-11-13 14:32:49 +0200

    add routing

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import { connectToMongo } from "./config/db";
 import exp from "express";
 import "dotenv/config";
+import userRouter from "./router/userRouter"
+import actionRouter from "./router/actionRouter"
+import orgRouter from "./router/orgRouter"
+import misseilRouter from "./router/missailRouter"
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +12,11 @@ const app = exp();
 app.use(exp.json());
 
 connectToMongo()
+
+app.use("/users",userRouter );
+app.use("/org", orgRouter);
+app.use("/missail", misseilRouter);
+app.use("/action", actionRouter);
 
 app.listen(PORT, () => {
     console.log(`server app and runing in port ${PORT}`);
