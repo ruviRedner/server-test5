02fe4e2da265dffb504fc203d3ec5fa584bc183f
ruviRedner <r.y.r7966@gmail.com>
2024-11-13 15:31:56 +0200

    fix func register and put bcrypt password

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,22 +3,25 @@ import { LoginDto, RegisterDto } from "../types/DTO/userDto";
 import User from "../models/userModel";
 import Jwt from "jsonwebtoken";
 import orgModel from "../models/orgModel";
-import  PayloadDto  from "../types/DTO/payload";
+import PayloadDto from "../types/DTO/payload";
 
 export const userLogin = async (user: LoginDto) => {
   try {
     const userFromDb = await User.findOne({ username: user.username }).lean();
+
     if (!userFromDb) throw new Error("User not found");
     const match = await compare(user.password, userFromDb.password);
+
     if (!match) throw new Error("Worng password");
-    const payload:PayloadDto =  {
-        userId: userFromDb._id,
-        role: userFromDb.role,
-        org: userFromDb.org,
-        username: userFromDb.username,
-      }
-    const token = Jwt.sign(payload,
-     
+    const payload: PayloadDto = {
+      userId: userFromDb._id,
+      role: userFromDb.role,
+      org: userFromDb.org,
+      username: userFromDb.username,
+    };
+    const token = Jwt.sign(
+      payload,
+
       process.env.JWT_SECRET as string,
       {
         expiresIn: "10m",
@@ -34,7 +37,7 @@ export const userLogin = async (user: LoginDto) => {
 export const userRegister = async (user: RegisterDto) => {
   try {
     const { username, password, org, location, role } = user;
-    if (!username || !password || !org  || !role) {
+    if (!username || !password || !org || !role) {
       throw new Error("Missing user data, all is require");
     }
 
@@ -42,18 +45,24 @@ export const userRegister = async (user: RegisterDto) => {
     user.password = hashP;
     const newUser = new User({
       username,
-      password,
+      password: hashP,
       org,
       location,
       role,
     });
-    if(role === "jowish"){
-        const organization = await orgModel.findOne({name:`${org} - ${location}`})
-        newUser.org = organization!
+    if (role === "jowish") {
+      const organization = await orgModel.findOne({
+        orgName: `${org} - ${location}`,
+      });
+      newUser.org = organization!;
+      console.log(organization);
     }
-    if(role === "terorist"){
-        const organization = await orgModel.findOne({name:newUser.org})
-        newUser.org = organization!
+    if (role === "terorist") {
+      console.log(org);
+
+      const organization = await orgModel.findOne({ orgName: org });
+      console.log(organization);
+      newUser.org = organization!;
     }
     return await newUser.save();
   } catch (error) {
