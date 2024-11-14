484c27230f6f2366a1aeae5a95aa920c32f3414b
ruviRedner <r.y.r7966@gmail.com>
2024-11-14 09:54:45 +0200

    change to return obj

--- src/controller/actionController.ts
+++ src/controller/actionController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
+import { handelAttack, handleDefense } from "../services/actionService";
+import { ActionDto } from "../types/DTO/actionDto";
 
 
-export const attack = async (req: Request, res: Response): Promise<void> => {
+export const attack = async (req: Request<any,any,ActionDto>, res: Response): Promise<void> => {
   try {
-    
-    res.sendStatus(201);
+   const result = await handelAttack(req.body)
+    res.status(201).json(result)
   } catch (error) {
     console.log(error);
     res.sendStatus(400);
@@ -12,8 +14,8 @@ export const attack = async (req: Request, res: Response): Promise<void> => {
 };
 export const intercept = async (req: Request, res: Response): Promise<void> => {
     try {
-      
-      res.sendStatus(201);
+     const result = await handleDefense(req.body);
+      res.status(201).json(result);
     } catch (error) {
       console.log(error);
       res.sendStatus(400);
