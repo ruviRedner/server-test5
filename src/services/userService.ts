e6437121d5b4c8a8263a3d0d90e58cd9c2725209
ruviRedner <r.y.r7966@gmail.com>
2024-11-14 09:56:13 +0200

    delete notes

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -34,38 +34,127 @@ export const userLogin = async (user: LoginDto) => {
   }
 };
 
+// export const userRegister = async (user: RegisterDto) => {
+//   try {
+//     const { username, password, org, location, role } = user;
+//     if (!username || !password || !org || !role) {
+//       throw new Error("Missing user data, all is require");
+//     }
+
+//     const hashP = await hash(user.password, 10);
+//     user.password = hashP;
+//     const newUser = new User({
+//       username,
+//       password: hashP,
+//       org,
+//       location,
+//       role,
+//     });
+//     if (role === "jowish") {
+//       const organization = await orgModel.findOne({
+//         orgName: `${org} - ${location}`,
+//       });
+//       newUser.org = organization!;
+//       console.log(organization);
+//     }
+//     if (role === "terorist") {
+//       console.log(org);
+
+//       const organization = await orgModel.findOne({ orgName: org });
+//       console.log(organization);
+//       newUser.org = organization!;
+//     }
+//     return await newUser.save();
+//   } catch (error) {
+//     throw new Error("Can't do the thing you want me to do");
+//   }
+// };
+// export const userRegister = async (user: RegisterDto) => {
+//   try {
+//     const { username, password, org, location, role } = user;
+
+//     if (!username || !password || !org || !role) {
+//       throw new Error("Missing user data, all fields are required");
+//     }
+
+//     const existingUser = await User.findOne({ username });
+//     if (existingUser) {
+//       throw new Error("Username is already taken");
+//     }
+
+//     const hashedPassword = await hash(password, 10);
+
+//     let organization;
+//     if (role === "jewish") {
+
+//       if (!location) throw new Error("Location is required for defense roles");
+//       organization = await orgModel.findOne({ orgName: `${org} - ${location}` });
+//     } else if (role === "terrorist") {
+
+//       organization = await orgModel.findOne({ orgName: org });
+//     }
+
+//     if (!organization) {
+//       throw new Error("Organization not found for the specified role and location");
+//     }
+
+//     const newUser = new User({
+//       username,
+//       password: hashedPassword,
+//       org: organization._id,
+//       role,
+//       ...(role === "jewish" && { location })
+//     });
+
+//     await newUser.save();
+//     return { message: "User registered successfully", user: newUser };
+//   } catch (error:Error | unknown) {
+//     throw new Error(`Registration failed: ${(error as Error).message}`);
+//   }
+// };
 export const userRegister = async (user: RegisterDto) => {
   try {
     const { username, password, org, location, role } = user;
+
     if (!username || !password || !org || !role) {
-      throw new Error("Missing user data, all is require");
+      throw new Error("Missing user data, all fields are required");
     }
 
-    const hashP = await hash(user.password, 10);
-    user.password = hashP;
-    const newUser = new User({
-      username,
-      password: hashP,
-      org,
-      location,
-      role,
-    });
-    if (role === "jowish") {
-      const organization = await orgModel.findOne({
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      throw new Error("Username is already taken");
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    let organization;
+    if (role === "jewish") {
+      if (!location) throw new Error("Location is required for defense roles");
+      organization = await orgModel.findOne({
         orgName: `${org} - ${location}`,
       });
-      newUser.org = organization!;
-      console.log(organization);
+    } else if (role === "terrorist") {
+      organization = await orgModel.findOne({ orgName: org });
     }
-    if (role === "terorist") {
-      console.log(org);
+ 
+    if (!organization) {
+      throw new Error(
+        "Organization not found for the specified role and location"
+      );
+    }
 
-      const organization = await orgModel.findOne({ orgName: org });
-      console.log(organization);
-      newUser.org = organization!;
-    }
-    return await newUser.save();
-  } catch (error) {
-    throw new Error("Can't do the thing you want me to do");
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      org: organization,
+      role,
+      ...(role === "jewish" && { location }),
+    });
+
+    await newUser.save();
+
+    return { message: "User registered successfully", user: newUser };
+  } catch (error: Error | unknown) {
+    throw new Error(`Registration failed: ${(error as Error).message}`);
   }
 };
