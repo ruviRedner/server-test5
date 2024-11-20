e3e538bb2523fdf6f4bd907d085f99d5be0bb3c9
ruviRedner <r.y.r7966@gmail.com>
2024-11-20 10:16:43 +0200

    change env

--- src/middleware/VerifyUser.ts
+++ src/middleware/VerifyUser.ts
@@ -1,37 +1,39 @@
 import { NextFunction, Request, Response } from "express";
-import jwt, { TokenExpiredError} from "jsonwebtoken";
+import jwt, { TokenExpiredError } from "jsonwebtoken";
 import RequestWithUser from "../types/interfacec/RequestWithUser";
 import TokenPayloadDTO from "../types/DTO/payload";
 
-
-
 const verifyUser = async (
-    req: RequestWithUser | Request,
-    res: Response,
-    next: NextFunction
+  req: RequestWithUser | Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    try {    
-        const token:string =  req.headers["Authorization"] as string;
-        console.log(token);
-        
-        const payload: TokenPayloadDTO = jwt.verify(token, process.env.TOKEN_SECRET as string) as TokenPayloadDTO
-        (req as RequestWithUser).user = payload
-        next()
-    } catch (err) {
-        if (err instanceof TokenExpiredError) {
-            res.status(401).json({
-                err: true,
-                message: "Token expiered, please log in again",
-                data: err
-            })
-        } else {
-            res.status(400).json({
-                err: true,
-                message: "Token is missing or curropted",
-                data: err
-            })
-        }
-    }
-}
+  try {
+    const token: string = req.headers["authorization"] as string;
 
-export default verifyUser
+    const payload: TokenPayloadDTO = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    )as TokenPayloadDTO
+    
+    (req as RequestWithUser).user = payload
+
+    next();
+  } catch (err) {
+    if (err instanceof TokenExpiredError) {
+      res.status(401).json({
+        err: true,
+        message: "Token expiered, please log in again",
+        data: err,
+      });
+    } else {
+      res.status(400).json({
+        err: true,
+        message: "Token is missing or curropted",
+        data: err,
+      });
+    }
+  }
+};
+
+export default verifyUser;
